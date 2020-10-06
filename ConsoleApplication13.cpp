25c3051808489f16e65a8fbbcb6e3abde7f4b6b1
Tikhonov-Vladimir <63228981+Tikhonov-Vladimir@users.noreply.github.com>
2020-10-06 15:05:24 +0300

    Add files via upload

---
+++ ConsoleApplication13.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n,max1=0,max2=0;
+    ifstream fin("C:\\test.txt");
+    fin >> n;
+    while (n != 0) {
+        if (n % 7 == 0 && n % 49 != 0 && n > max1) {
+            max1 = n;
+        }
+        if (n % 7 != 0 && n > max2) {
+            max2 = n;
+        }
+        fin >> n;
+    }
+    cout << max1 * max2;
+    return 0;
+}
+
